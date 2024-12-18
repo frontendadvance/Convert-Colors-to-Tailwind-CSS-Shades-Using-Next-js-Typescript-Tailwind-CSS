e3ee14bdf7f64ee110fe0f1ad95ffebdfcd22274
Raja Charles <raja.charles@cegeka.com>
2024-12-18 17:38:58 +0530

    Convert Colors to Tailwind Shades

---
+++ components/pages/home-page.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import React, { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Alert } from "@/components/ui/alert";
+import { TAILWIND_COLORS } from "@/constants/tailwind-colors";
+import { cn } from "@/lib/utils";
+import { toast } from "sonner";
+import { Copy } from "lucide-react";
+
+
+const HomePage: React.FC = () => {
+
+  return (
+    <div>
+      Home
+    </div>
+  );
+};
+
+export default HomePage;
